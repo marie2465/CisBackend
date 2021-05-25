847227dc50299f62fcea3fdcdf3a05c7d9da1593
Мария Янголь <marie.yangol2465@gmail.com>
2021-05-25 13:35:14 +0200

    criterion in hosting

--- Data/CisDBContext.cs
+++ Data/CisDBContext.cs
@@ -6,6 +6,7 @@ namespace Cis_part2.Data
     public class CisDBContext : DbContext
     {
         public CisDBContext(DbContextOptions<CisDBContext> options) : base(options) { }
+        public DbSet<Criteries> Criteries { get; set; }
         public DbSet<Roles> Roles { get; set; }
         public DbSet<Sections> Sections { get; set; }
         public DbSet<Skills> Skills { get; set; }
---
+++ Migrations/20210525112422_Criteries.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cis_part2.Migrations
+{
+    public partial class Criteries : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Criteries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Letter = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Mark = table.Column<double>(type: "float", nullable: false),
+                    SkillsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Criteries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Criteries_Skills_SkillsId",
+                        column: x => x.SkillsId,
+                        principalTable: "Skills",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Criteries_SkillsId",
+                table: "Criteries",
+                column: "SkillsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Criteries");
+        }
+    }
+}
